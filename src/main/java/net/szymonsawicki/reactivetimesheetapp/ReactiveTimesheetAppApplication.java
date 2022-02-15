94bb26e3af848c7734dbfd1552ac1c876b85d90a
szsa <szymon.sawicki@cpb-software.com>
2022-02-15 07:52:01 +0100

    initial commit

---
+++ src/main/java/net/szymonsawicki/reactivetimesheetapp/ReactiveTimesheetAppApplication.java
@@ -0,0 +1,13 @@
+package net.szymonsawicki.reactivetimesheetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTimesheetAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveTimesheetAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/szymonsawicki/reactivetimesheetapp/ReactiveTimesheetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.szymonsawicki.reactivetimesheetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveTimesheetAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
