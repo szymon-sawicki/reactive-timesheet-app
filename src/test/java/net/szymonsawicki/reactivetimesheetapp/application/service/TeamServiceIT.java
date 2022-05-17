340d3b2a8a0607381073c81a609c26248ae36d7e
szsa <szymon.sawicki@cpb-software.com>
2022-05-17 22:47:41 +0200

    introduces spring profiles and prepares IT tests, cleans up pom.xml

---
+++ src/test/java/net/szymonsawicki/reactivetimesheetapp/application/service/TeamServiceIT.java
@@ -0,0 +1,67 @@
+package net.szymonsawicki.reactivetimesheetapp.application.service;
+
+import net.szymonsawicki.reactivetimesheetapp.application.service.utils.TimesheetAppMongoDbContainer;
+import net.szymonsawicki.reactivetimesheetapp.domain.team.Team;
+import net.szymonsawicki.reactivetimesheetapp.domain.team.TeamUtils;
+import net.szymonsawicki.reactivetimesheetapp.domain.team.repository.TeamRepository;
+import net.szymonsawicki.reactivetimesheetapp.domain.user.User;
+import net.szymonsawicki.reactivetimesheetapp.domain.user.repository.UserRepository;
+import net.szymonsawicki.reactivetimesheetapp.domain.user.type.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+
+@SpringBootTest
+@Testcontainers
+@WebFluxTest
+@ActiveProfiles("test")
+public class TeamServiceIT {
+    @Container
+    private static final MongoDBContainer MONGO_DB_CONTAINER = TimesheetAppMongoDbContainer.getInstance();
+
+    @Autowired
+    private TeamRepository teamRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private WebTestClient WebTestClient;
+
+    @Test
+    void shouldReturnTeamOnGetById() {
+
+        String userId = "21344r23r34";
+        String teamId = "2r872394r578";
+        String teamName = "Some team";
+        String username = "testsusrname";
+        Role role = Role.DEVELOPER;
+
+        var member = User.builder()
+                .username(username)
+                .password("some password")
+                .teamId(teamId)
+                .build();
+
+        var team = Team.builder()
+                .name(teamName)
+                .members(List.of(member))
+                .build();
+
+        var insertedTeam = teamRepository.save(team);
+
+        var insertedTeamId = TeamUtils.toId.apply(insertedTeam.block());
+
+        StepVerifier.create(teamRepository.findById(insertedTeamId))
+                .expectNextMatches(t -> TeamUtils.toMembers.apply(t).size() == 1)
+                .verifyComplete();
+    }
+}
